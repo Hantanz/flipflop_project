b0053e95085d314350764d143ceac3ae8ae20626
DrMadhushan <dev.drmad@gmail.com>
2022-08-20 22:34:05 +0530

    Fix issues in visibility

--- flipflop/src/main/java/com/flipflop/flipflop/HelloApplication.java
+++ flipflop/src/main/java/com/flipflop/flipflop/HelloApplication.java
@@ -3,7 +3,9 @@ package com.flipflop.flipflop;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.SubScene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,8 +13,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        stage.setTitle("FlipFlop - Production Quality Analyser");
+        stage.initStyle(StageStyle.DECORATED); // not necessary 'UNDECORATED can remove the top menu bar
         stage.setScene(scene);
         stage.show();
     }
