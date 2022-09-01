8459e0b5fc34d0a45f2bd76bf247577ba5cb3224
DrMadhushan <dev.drmad@gmail.com>
2022-09-01 07:07:28 +0530

    update packages

--- flipflop/src/main/java/com/flipflop/flipflop/HelloApplication.java
+++ flipflop/src/main/java/com/flipflop/flipflop/HelloApplication.java
@@ -3,7 +3,6 @@ package com.flipflop.flipflop;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.SubScene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
